605a6a8a74df737e88916cafbe38c4f059bdc9a6
Joachim Laviolette <joachim.laviolette@gmail.com>
2018-01-09 23:01:09 +0100

    Delete StrategieDeJeu.java

--- src/StrategieDeJeu.java
+++
@@ -1,9 +0,0 @@
-public interface StrategieDeJeu 
-{
-	public int choisirAction();
-	public Carte choisirCarte();
-	public Carte choisirSymboleCarteApresHuit();
-	public Carte choisirCouleurCarteApresHuit();
-	public String choisirCarteSupplement();
-	public String proposerAjouterCarte();
-}
